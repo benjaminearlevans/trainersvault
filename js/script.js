6e8e58219a374c2902a57d1ae6fe82d07ee25fa6
Abbdul Muis Al Amin <abdul.muis56@gmail.com>
2017-07-12 18:05:12 +0700

    Bug fix and add client page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+
+})
+
+$(window).scroll(function(){
+	if($("header .dropdown.yamm-fw").hasClass("open")){
+		$("header .dropdown.yamm-fw a").trigger("click");
+	}
+})
+
 $(document).on('click', '.yamm .dropdown-menu', function(e) {
   e.stopPropagation()
 })
